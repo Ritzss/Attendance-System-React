563a965c48ac45fa66ad601b159af96b59c20ad9
Ritzss <ritanshubabuta399@gmail.com>
2025-12-10 10:35:23 +0530

    second comit

--- src/pages/Services/Contactus.jsx
+++ src/pages/Services/Contactus.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import AuthLogo from '../../components/Auth/AuthLogo'
 
 const Contactus = () => {
   return (
-    <div>Contactus</div>
+    <section className='h-screen flex-col flex justify-evenly items-center gap-15'>
+      <header className='flex h-[40%] justify-center '>
+        <AuthLogo className={"w-[98%]  blue-metal rounded-3xl px-40 flex-col flex justify-evenly items-center"}/>
+      </header>
+      <main className=' w-[98%] h-[10%] border'></main>
+      <footer className= 'w-full sticky top-[80%] z-10 bg-black '>
+        <div className="bg-white w-[20vh] h-[20vh]"></div>
+      </footer>
+    </section>
   )
 }
 
